a1b216ae58ede02c91fc086739afeafa9f92f3d3
Tom_XV <Tom_XV@outlook.jp>
2024-01-21 03:22:19 +0900

    Update Palworld.sh

--- Palworld.sh
+++ Palworld.sh
@@ -5,6 +5,8 @@ sudo apt update ;
 sudo apt upgrade -y ;
 apt install steamcmd screen -y ;
 steamcmd +login anonymous +app_update 2394010 validate +quit ;
-ln -s ~/Steam/steamapps/common/PalServer/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
+mkdir -p ~/.steam/sdk64/ ;
+steamcmd +login anonymous +app_update 1007 +quit ;
+ln -s ~/Steam/steamapps/common/PalServer/linux64/steamclient.so ~/.steam/sdk64/steamclient.so ;
 cd ~/Steam/steamapps/common/PalServer ;
 screen -dmS Palworld-Server ./PalServer.sh
